ad6ac43f83e6180931f9f1e73394f69453006582
Fatah <fertahrez@gmail.com>
2024-07-23 10:13:22 +0300

    add scss style for homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 import * as data from './../Model/data.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
